d07ab531bca0000dbaf5f06dacdaa19330238611
Bhaskar Goyal <bhaskargoyal24@gmail.com>
2017-01-15 22:20:04 +0530

    added mute button

---
+++ script/muteButton.js
@@ -0,0 +1,16 @@
+$('.speaker').click(function(e) {
+  e.preventDefault();
+  var audioba = document.getElementById("background-ambience");
+  var audiobb = document.getElementById("hover-menu-sound");
+	if( $(this).hasClass('mute') == true) {
+		// console.log("has mute");
+		audioba.volume = 1;
+		audiobb.volume = 1;
+		$(this).toggleClass('mute');
+	} else {
+		// console.log("no mute");
+		audioba.volume = 0;
+		audiobb.volume = 0;
+		$(this).toggleClass('mute');
+	}
+});
